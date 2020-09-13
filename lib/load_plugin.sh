64d69a9ddb1850552c2c2eb6351e72d7034e924a
Tim Anido <xanido@gmail.com>
2020-09-13 22:21:06 +1000

    fix plugin loader

--- lib/load_plugin.sh
+++ lib/load_plugin.sh
@@ -1,10 +1,11 @@
+# vim: set ft=bash
 bashed::load_plugin() {
     local plugin_path=$1
     local plugin_name=$(basename "$plugin_path")
     local plugin_disabled_flag="BASHED_DISABLE_PLUGIN_$(bashed::strtoupper $plugin_name)"
     local plugin_disabled=${!plugin_disabled_flag:=0}
     bashed::log::debug "$plugin_disabled_flag: $plugin_disabled"
-    bashed::log::info "Loading plugin $plugin_name from $plugin_path"
+    bashed::log::debug "Loading plugin $plugin_name from $plugin_path"
     if [[ "${!plugin_disabled_flag}" == 1 ]]; then
         bashed::log::debug "not loading bashed plugin $plugin_name (reason: \$$plugin_disabled_flag)"
         return
